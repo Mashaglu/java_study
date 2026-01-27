1adcb685b400c0fd1fb2bc9e3620bd4acc0a1478
Masha <masha.gluu@gmail.com>
2026-01-27 20:04:32 +0300

    работа с числами

---
+++ java lesson/src/main/java/lesson1/Main.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        //Числа
+        byte byteVar = 1;
+        short shortVar = 2;
+        int intVar = 3;
+        long longVar = 4;
+
+
+        //float
+        float floatVar = 123.45f;
+        double doubleVar = 123.456d;
+
+        var number =1234;
+        // + - * / %
+
+        var result = 100/10000; // так выдаст в результате только целое число
+        var result1 = 100/10000.0; //или
+        double result2 = 100/10000.0;
+
+        System.out.println(result2);
+    }
+}
