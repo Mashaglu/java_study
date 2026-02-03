5a82296b7705110be937cf69973f494a1a1bb0a1
Masha <masha.gluu@gmail.com>
2026-02-03 20:03:27 +0300

    lesson 3

---
+++ java lesson/src/main/java/lesson3/Main.java
@@ -0,0 +1,43 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        final Person person = new Person();
+        person.setFirstName("John");
+        final String name = person.getFirstName();
+        System.out.println(person.getFirstName());
+        System.out.println(name);
+
+        final Person person1 = new Person("Иван", "Иванов");
+        final String name1 = person1.getFirstName();
+        person1.setAge(22);
+        final int age = person1.getAge();
+        person1.voice();
+        System.out.println(name1);
+
+        // поле className принадлежит классу Person а не инстансах (объектах)
+        // созданных на основе класса Person
+        System.out.println(Person.className);
+
+        // модификатор final
+        final Person person2 = new Person();
+        // Ошибка person2 не изменяемый, нельзя переписать
+//        person2 = person;
+
+        // переменная number неизменяемая - константа
+        final int number = 100;
+//        number = 1;
+
+        final User user = new User("Петя", "Петров","12345");
+        System.out.println(user.getFirstName() + " " + user.getLastName() + " " + user.getPassword());
+
+
+
+
+        }
+
+
+
+
+
+}
---
+++ java lesson/src/main/java/lesson3/User.java
@@ -0,0 +1,30 @@
+package lesson3;
+
+public class User {
+
+    // поле константа, при условии его инициализации в конструкторе
+    private final  String firstName;
+    // поле константа, при условии его инициализации в конструкторе
+    private final String lastName;
+    private final String password;
+
+
+    public User(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+    // Нет возможности создать конструктор без параметров
+    //    public User() {
+    //    }
+
+    // Есть только возможность генерировать Getters
+    // по скольку поля присвоят значения при создании объекта через конструктор
+    public String getFirstName() {return firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public String getPassword() {return "******";}
+
+
+}
